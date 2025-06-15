9a4715db3c0719ebcaa6a11acb343026a12a6435
facundocom <facundocoriia@gmail.com>
2025-06-15 02:35:56 -0300

    Fix error inciso 1

--- aplicacionSQLAlchemy/app.py
+++ aplicacionSQLAlchemy/app.py
@@ -32,29 +32,29 @@ def registrarentrada():
             else:
                 if (trabajador.dni)[-4:] != request.form['dni']:
                     return render_template('error.html', error="DNI incorrecto, ingrese un DNI válido.")
-               
+                
                 else:
                     registro = RegistroHorario.query.filter_by(
                         fecha=datetime.now().date(),
-                        idtrabajador=trabajador.legajo,
+                        idtrabajador=trabajador.id,
                     ).first()
 
-                    if registro is not None:
+                    if registro:
                         return render_template('error.html', error="Ya se registró una entrada para hoy, por favor intentélo de nuevo mañana, no se olvide de registrar su salida.")
-
-                    nuevo_registro = RegistroHorario(
-                        fecha=datetime.now().date(),
-                        horaentrada=datetime.now().time(),
-                        horasalida=None,
-                        dependencia= request.form['dependencia'],
-                        idtrabajador=trabajador.id
-                        )
- 
-                    db.session.add(nuevo_registro)
-                    db.session.commit()
-                    return render_template('aviso.html', mensaje="¡Entrada registrada exitosamente! Buen día :).")
-                   
-    return render_template('registrar_entrada.html')
+                    else:
+                        nuevo_registro = RegistroHorario(
+                            fecha=datetime.now().date(),
+                            horaentrada=datetime.now().time(),
+                            horasalida=None,
+                            dependencia= request.form['dependencia'],
+                            idtrabajador=trabajador.id
+                            )
+    
+                        db.session.add(nuevo_registro)
+                        db.session.commit()
+                        return render_template('aviso.html', mensaje="¡Entrada registrada exitosamente! Buen día :).")
+    else:
+        return render_template('registrar_entrada.html')
 
 
 @app.route('/registrar_salida', methods=['GET', 'POST'])
@@ -71,29 +71,31 @@ def registrarsalida():
             registro.horasalida = datetime.now().time()
             db.session.commit()
             return render_template('aviso.html', mensaje="¡Salida registrada exitosamente! Nos vemos pronto :)")
-        
-        legajo = request.form.get('legajo')
-        dni = request.form.get('dni')
-        trabajador = Trabajador.query.filter_by(legajo=legajo).first()
-        
-        if trabajador is None:
-            return render_template('error.html', error="Trabajador no encontrado, ingrese un legajo válido.")
-        
-        else:
-            if (trabajador.dni)[-4:] != dni:
-                return render_template('error.html', error="DNI incorrecto, ingrese un DNI válido.")
+        else: 
+            legajo = request.form.get('legajo')
+            dni = request.form.get('dni')
+            trabajador = Trabajador.query.filter_by(legajo=legajo).first()
             
-            registro = RegistroHorario.query.filter_by(
-                idtrabajador=trabajador.id,
-                fecha=datetime.now().date(),
-                horasalida=None
-            ).first()
+            if trabajador is None:
+                return render_template('error.html', error="Trabajador no encontrado, ingrese un legajo válido.")
             
-            if registro is None:
-                return render_template('error.html', error="No se encontró un registro de entrada para hoy, por favor registre su entrada primero.")
-            return render_template('confirmar_salida.html', dependencia=registro.dependencia, registro_id=registro.id, legajo=trabajador.legajo, dni=trabajador.dni)
-            
-    return render_template('registrar_salida.html')
+            else:
+                if (trabajador.dni)[-4:] != dni:
+                    return render_template('error.html', error="DNI incorrecto, ingrese un DNI válido.")
+                else:    
+                    registro = RegistroHorario.query.filter_by(
+                        idtrabajador=trabajador.id,
+                        fecha=datetime.now().date(),
+                        horasalida=None
+                    ).first()
+                    
+                    if registro is None:
+                        return render_template('error.html', error="No se encontró un registro de entrada para hoy, por favor registre su entrada primero.")
+                    else:
+                        return render_template('confirmar_salida.html', dependencia=registro.dependencia, registro_id=registro.id, legajo=trabajador.legajo, dni=trabajador.dni)
+
+    else:        
+        return render_template('registrar_salida.html')
 
 
 @app.route('/consultar', methods=['GET', 'POST'])
@@ -114,18 +116,20 @@ def consultar():
         else:
             if (trabajador.dni)[-4:] != dni:
                 return render_template('error.html', error="DNI incorrecto, ingrese un DNI válido.")
-            
-            registros = RegistroHorario.query.filter(
-                RegistroHorario.idtrabajador == trabajador.id,
-                RegistroHorario.fecha >= fecha_inicio,
-                RegistroHorario.fecha <= fecha_fin
-                ).order_by(RegistroHorario.fecha.desc()).all()
+            else:    
+                registros = RegistroHorario.query.filter(
+                    RegistroHorario.idtrabajador == trabajador.id,
+                    RegistroHorario.fecha >= fecha_inicio,
+                    RegistroHorario.fecha <= fecha_fin
+                    ).order_by(RegistroHorario.fecha.desc()).all()
 
-            if not registros:
-                return render_template('error.html', error="No se encontraron registros laborales en esas fechas.")
-            return render_template('consultar.html', registros=registros, trabajador=trabajador)
+                if not registros:
+                    return render_template('error.html', error="No se encontraron registros laborales en esas fechas.")
+                else:
+                    return render_template('consultar.html', registros=registros, trabajador=trabajador)
 
-    return render_template('consultar.html')#
+    else:
+        return render_template('consultar.html')#
 
 if __name__ == '__main__':
     with app.app_context():
